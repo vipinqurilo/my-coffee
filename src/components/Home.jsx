9c59ace7bfa48f3890754be784806f3d23b36fd5
Sadhana Rathour <sadhana@qurilo.com>
2024-08-07 14:54:43 +0530

    asdf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,40 @@
+import { useState, useEffect } from "react";
+import Loader from "./Loader";
+import AboutSection from "./Home/AboutSection";
+import Footer from "./Home/Footer";
+import HeroSection from "./Home/HeroSection";
+import OurStory from "./Home/OurStory";
+import Products from "./Home/Products";
+import DiscountPage from "./Home/DiscountPage";
+import SpecialsOfTheDay from "./Home/SpecialsOfTheDay";
+import Testimonials from "./Home/Testimonials";
 
-import AboutSection from "./Home/AboutSection"
-import Footer from "./Home/Footer"
-import HeroSection from "./Home/HeroSection"
-import OurStory from "./Home/OurStory"
-import Products from "./Home/Products"
-import DiscountPage from "./Home/DiscountPage"
-import SpecialsOfTheDay from "./Home/SpecialsOfTheDay"
-import Testimonials from "./Home/Testimonials"
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a loading time with a timeout. You can replace this with real data fetching logic.
+    const timer = setTimeout(() => setLoading(false), 2000);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="bg-[#242424]">
-        <HeroSection/>
-        <OurStory/>
-        <Products/>
-        <AboutSection/>
-        <SpecialsOfTheDay/>
-        <DiscountPage/>
-        
-        <Testimonials/>
-        <Footer/>
-
+      <HeroSection />
+      <OurStory />
+      <Products />
+      <AboutSection />
+      <SpecialsOfTheDay />
+      <DiscountPage />
+      <Testimonials />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
