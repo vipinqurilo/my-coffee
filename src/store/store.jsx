3678d2673970346a273ff44ff13697d9752ad785
Sadhana Rathour <sadhana@qurilo.com>
2024-08-10 11:12:12 +0530

    Revert "fsda"
    This reverts commit 0a6b2cfc70b0401eb298afdf0764942b9ac9f1bc.

--- src/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './Reducers/Reducer'
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-})
